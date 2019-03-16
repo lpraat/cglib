0ad84ec49b8d0f531dd733654953a8070863e01f
lpraat <pratissolil@gmail.com>
2019-03-17 00:45:39 +0100

    Fixed glad

--- learnopengl/main.cpp
+++ learnopengl/main.cpp
@@ -5,6 +5,37 @@
 
 int main() {
 
-    printf("ciao");
+    // Initialize glfw
+    glfwInit();
+
+    // Set some options
+    // See https://www.glfw.org/docs/latest/window.html#window_hints
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    // Create a window object that holds all the windowing data
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    // We pass GLAD the function to load the address of the OpenGL function pointers
+    // which is OS-specific. GLFW gives us glfwGetProcAddress that defines the correct
+    // function based on which OS we're compiling for.
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+
+
+    return 0;
 
 }
